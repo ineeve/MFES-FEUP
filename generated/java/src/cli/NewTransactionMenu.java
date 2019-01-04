5acd0e48f9971cb1a6c2fa26649718da48506abb
Joao611 <joaofvde@gmail.com>
2019-01-04 22:17:06 +0000

    CLI progress

--- generated/java/src/cli/NewTransactionMenu.java
+++ generated/java/src/cli/NewTransactionMenu.java
@@ -1,5 +1,7 @@
 package cli;
 
+import org.overture.codegen.runtime.VDMSet;
+
 import instance.Kid2KidSingleton;
 import vdm.Kid2Kid;
 
@@ -7,13 +9,18 @@ public class NewTransactionMenu extends Menu {
 
 	private String cashierName;
 	private Kid2Kid kid2kid = Kid2KidSingleton.getInstance();
+	private VDMSet clients;
 	
 	public NewTransactionMenu(String cashierName) {
 		this.cashierName = cashierName;
+		this.clients = kid2kid.getClients();
 	}
 	
 	@Override
 	public void initialize() {
-		
+		clients.forEach((client) -> {
+			System.out.println(client);
+			System.out.println(client.getClass().getName());
+		});
 	}
 }
--- generated/java/src/instance/Kid2KidFactory.java
+++ generated/java/src/instance/Kid2KidFactory.java
@@ -2,10 +2,12 @@ package instance;
 
 import vdm.Kid2Kid;
 import vdm.Store;
+import vdm.StoreCashier;
 
 public class Kid2KidFactory {
 	
 	private static final String[] stores = {"Porto", "Lisboa", "Aveiro"};
+	private static final String cashierName = "Alberto";
 	
 	public static Kid2Kid getKid2Kid() {
 		Kid2Kid kid2kid = new Kid2Kid();
@@ -18,8 +20,10 @@ public class Kid2KidFactory {
 	}
 
 	private static void addStores(Kid2Kid kid2kid) {
-		for (String store : stores) {
-			kid2kid.addStore(new Store(store));
+		for (String storeName : stores) {
+			Store store = new Store(storeName);
+			store.addCashier(new StoreCashier(cashierName, store));
+			kid2kid.addStore(new Store(storeName));
 		}
 	}
 }
